7b0be8f3b8e31746d714590e891dfee9344a8643
Khamphay <txammuas1@gmail.com>
2024-04-27 14:41:44 +0700

    commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__, static_folder="static")
+
+
+@app.route("/")
+def hello():
+    data = "Coopsoil Lab"
+    return render_template("index.html", data=data)
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/data", methods=["POST"])
+def data():
+    data = request.form
+    return render_template("data.html", data=data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
