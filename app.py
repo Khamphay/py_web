2a7388db11cc6454f62463419ef60178c85a8e61
Khamphay <txammuas1@gmail.com>
2024-05-16 16:37:10 +0700

    update

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import time
 from threading import Thread, Event
 import csv
 import os
+from datetime import datetime
 
 app = Flask(__name__, static_folder="static")
 Compress(app)
@@ -15,62 +16,82 @@ val1 = 10
 val2 = 12
 # stop_event: Event
 
+condition = lambda d1, d2: d1 == d2
+
 
 def stop_thread():
     global stop_event
     stop_event.set()
 
 
-def read_csv_file(start_line: int, end_line: int):
+def read_csv_file(start_line: int, end_line: int, date):
     json_data = []
     total = 0
     with open(os.path.join(os.getcwd(), "static/data_test.csv"), "r") as file:
+        lines = csv.reader(file, delimiter=",")
+        if date is not None:
+            total = sum(1 for l in lines if condition(date, l[0].split(" ")[0]))
+        else:
+            total = sum(1 for l in lines)
+        file.close()
+    with open(os.path.join(os.getcwd(), "static/data_test.csv"), "r") as file:
         csv_data = csv.reader(file, delimiter=",")
         for i, row in enumerate(csv_data):
+            if date is not None and not condition(date, row[0].split(" ")[0]):
+                continue
             if i < start_line:
                 continue
             if i > end_line:
                 break
             data_dict = {
                 "date": row[0],
-                "diam1": row[1],
-                "diam2": row[2],
-                "height": row[3],
-                "wieght": row[4],
-                "emiss1": row[5],
-                "emiss2": row[6],
-                "airh1": row[7],
-                "airh2": row[8],
-                "temp1": row[9],
-                "temp2": row[10],
-                "temp3": row[11],
+                "diam1": float(row[1]),
+                "diam2": float(row[2]),
+                "height": float(row[3]),
+                "wieght": float(row[4]),
+                "emiss1": float(row[5]),
+                "emiss2": float(row[6]),
+                "airh1": float(row[7]),
+                "airh2": float(row[8]),
+                "temp1": float(row[9]),
+                "temp2": float(row[10]),
+                "temp3": float(row[11]),
             }
             json_data.append(data_dict)
-        total = sum(1 for _ in csv_data)
         file.close()
     return {"data": json_data, "total": total}
 
 
 @app.route("/", methods=["GET"])
 def hello():
-    page = request.args.get("page", 1, type=int)
-    per_page = request.args.get("perPage", 500, type=int)
-    end = per_page * page
-    start = (end - per_page) + 1
-    json_data = read_csv_file(start, end)
-    json_data["page"] = page
-    json_data["perPage"] = per_page
-    return render_template("index.html", data=json_data, page=4)
+    # page = request.args.get("page", 1, type=int)
+    # per_page = request.args.get("perPage", 500, type=int)
+    # end = per_page * page
+    # start = (end - per_page) + 1
+    # json_data = read_csv_file(start, end)
+    # json_data["page"] = page
+    # json_data["perPage"] = per_page
+    return render_template("index.html")
 
-@app.route("/data", methods=["POST"])
+
+@app.route("/data", methods=["POST", "GET"])
 def data():
+
+    date = request.args.get("date", None)
+    if date is not None and date != "null":
+        orig_date = datetime.strptime(date, "%Y-%m-%d")
+        date = datetime.strftime(orig_date, "%d-%m-%Y")
+    else:
+        date = None
     page = request.args.get("page", 1, type=int)
     per_page = request.args.get("perPage", 500, type=int)
     end = per_page * page
     start = (end - per_page) + 1
-    json_data = read_csv_file(start, end)
+
+    json_data = read_csv_file(start, end, date)
     json_data["page"] = page
     json_data["perPage"] = per_page
+
     return jsonify(json_data)
 
 
